a2e670f4cf7bd15d59be109f2b78e78787b7d1f3
Bertrand Lecervoisier <miniwark@gmail.com>
2014-02-03 11:50:26 +0100

    Still bootstraping the plugin

--- templates/settings.php
+++ templates/settings.php
@@ -5,3 +5,12 @@
 
 
 ?>
+
+<div class="wrap">
+    <h2><?php _e('Space API settings', 'wp-hackerspace') ?></h2>
+    <form action="options.php" method="post">
+        <?php // settings_fields('spaceapi'); ?>
+
+        <?php submit_button(); ?>
+    </form>
+</div>
--- wp-hackerspace.php
+++ wp-hackerspace.php
@@ -15,9 +15,61 @@
 
 class WPHackerspace
 {
+    // class contructor
     public function __construct()
     {
+        // load translations
+        load_plugin_textdomain('wp-hackerspace', false, plugin_dir_path(__FILE__).'/languages');
+
+        //
+        add_action('admin_init', array($this, 'admin_init'));
+
+        // enable the admin setting menu
+        add_action('admin_menu', array($this, 'admin_menu'));
     }
+
+    // activate the plugin
+    public static function activate()
+    {
+    }
+
+    // deactivate the plugin
+    public static function deactivate()
+    {
+    }
+
+    // register the plugin settings
+    public function admin_init()
+    {
+    }
+
+    // configure the plugin settings menu
+    public function admin_menu()
+    {
+            add_options_page(
+            __('Hackerspace', 'wp-hackerspace'),
+            __('Hackerspace', 'wp-hackerspace'),
+            'manage_options',
+            'hackerspace_options',
+            array($this, 'plugin_settings_template')
+        );
+    }
+
+    // render the settings template
+    public function plugin_settings_template()
+    {
+        if (! current_user_can('manage_options')) {
+            wp_die(__('You do not have sufficient permissions to access this page.', 'wp-hackerspace'));
+        }
+        include(sprintf(plugin_dir_path(__FILE__).'templates/settings.php'));
+    }
+
 }
 
+
+// register activation and deactivation hooks
+register_activation_hook(__FILE__, array('WPHackerspace', 'activate'));
+register_deactivation_hook(__FILE__, array('WPHackerspace', 'deactivate'));
+
+// instantiate the plugin class
 $wpHackerspace = new WPHackerspace();
