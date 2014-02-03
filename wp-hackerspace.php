2bf6d7477a07b450bfe7f12a579d1e8eb30016eb
Bertrand Lecervoisier <miniwark@gmail.com>
2014-02-03 10:19:53 +0100

    Bootstrap the plugin

---
+++ wp-hackerspace.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Hackerspace
+ * Plugin URI: https://github.com/nicelab/wp-hackerspace
+ * Author: Nicelab.org
+ * Author URI: http://nicelab.org/
+ * Description: Add custom post types useful for hackerspaces and expose informations trough the Space API.
+ * Version: 0.1
+ * Copyright: (c) 2014 Nicelab.org
+ * License: Expat/MIT License
+ * Text Domain: wp-hackerspace
+ * Domain Path: /languages
+ */
+
+
+class WPHackerspace
+{
+    public function __construct()
+    {
+    }
+}
+
+$wpHackerspace = new WPHackerspace();
